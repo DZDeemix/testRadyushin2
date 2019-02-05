c9383a534b092e8677189b63671d456018eac057
z.Dmitty <zyablikovdmitry@gmail.com>
2019-02-05 17:20:00 +0400

    edit Library class for use PDo extension

--- code/Library.php
+++ code/Library.php
@@ -14,7 +14,7 @@
 
 namespace App;
 
-use App\ConnectionDb;
+use App\models\LibraryModel;
 
 /**
  * Class Library
@@ -29,14 +29,14 @@ use App\ConnectionDb;
  */
 class Library
 {
-    private $_db;
+    private $_model;
 
     /**
      * Library constructor.
      */
     public function __construct()
     {
-        $this->_db = new ConnectionDb();
+        $this->_model = new LibraryModel();
     }
 
     /**
@@ -52,28 +52,8 @@ class Library
             echo "Введите название жанра";
             exit;
         }
-
-        $db = $this->_db->getConnection();
-
-        try {
-            //выбираем жанры автора
-            $autors = mysqli_query(
-                $db,
-                "SELECT DISTINCT a.name 
-            FROM books 
-            INNER JOIN authors a on books.author_id = a.id 
-            INNER JOIN genres g on books.genre_id = g.id
-            WHERE g.name = '${ganre}'"
-            );
-
-            $autors = mysqli_fetch_all($autors, MYSQLI_ASSOC);
-
-            //вывод в консоль
-            echo "Autor" . "\n";
-            $this->printArray($autors);
-        } catch (Exception $e) {
-            return ['errorMessage' => $e->getMessage()];
-        }
+        $result = $this->_model->getAutorsByGenre($ganre);
+        $this->printArray($result);
     }
 
     /**
@@ -83,26 +63,11 @@ class Library
      */
     public function getAutorsByCountBooks()
     {
-        $db = $this->_db->getConnection();
+        $result = $this->_model->getAutorsByCountBooks();
 
-        try {
-            //выбираем жанры автора
-            $autors = mysqli_query(
-                $db,
-                "SELECT distinct (a.name), count(a.name) as count 
-            FROM books b 
-            INNER JOIN authors a on b.author_id = a.id
-            GROUP BY a.name
-            ORDER BY count DESC"
-            );
-            $autors = mysqli_fetch_all($autors, MYSQLI_ASSOC);
-
-            //вывод в консоль
-            echo "Autor" . "\t" . "\t" . "Count" . "\n";
-            $this->printArray($autors);
-        } catch (Exception $e) {
-            return ['errorMessage' => $e->getMessage()];
-        }
+        //вывод в консоль
+        echo "Autor" . "\t" . "\t" . "Count" . "\n";
+        $this->printArray($result);
     }
 
     /**
@@ -118,27 +83,11 @@ class Library
             echo "Введите имя автора";
             exit;
         }
+        $result = $this->_model->getAutorsByGenreSortByRating($autor);
 
-        $db = $this->_db->getConnection();
-
-        try {
-            //выбираем жанры автора
-            $autors = mysqli_query(
-                $db,
-                "SELECT DISTINCT a.name, AVG(books.rating) as rating 
-            FROM books 
-            INNER JOIN authors a on books.author_id = a.id 
-            INNER JOIN genres g on books.genre_id = g.id
-            WHERE g.name = '${autor}'
-            GROUP BY a.name
-            ORDER BY rating DESC"
-            );
-            //вывод в консоль
-            echo "Autor" . "\t" . "\t" . "Rating" . "\n";
-            $this->printArray($autors);
-        } catch (Exception $e) {
-            return ['errorMessage' => $e->getMessage()];
-        }
+        //вывод в консоль
+        echo "Autor" . "\t" . "\t" . "Rating" . "\n";
+        $this->printArray($result);
     }
 
     /**
@@ -155,51 +104,10 @@ class Library
             exit;
         }
 
-        $db = $this->_db->getConnection();
-
-        try {
-            //выбираем жанры автора
-            $genres = mysqli_query(
-                $db,
-                "SELECT DISTINCT (g.id) 
-            FROM books b 
-            INNER JOIN authors a on b.author_id = a.id 
-            INNER JOIN genres g on b.genre_id = g.id
-            WHERE a.name = '${autor}'"
-            );
-            $genres = mysqli_fetch_all($genres, MYSQLI_ASSOC);
-
-            if (!empty($genres)) {
-                $where = "";
-                foreach ($genres as $genre) {
-                    $where = $where . " b.genre_id = " . $genre["id"] . " OR";
-                }
-                $where = substr($where, 0, -2);
-
-                /*выбираем похожих авторов по жанрам
-                (если совпадающих жанров больше значит
-                автор больше похож на заданного автора)*/
-                $autors = mysqli_query(
-                    $db,
-                    "SELECT DISTINCT (a.name), COUNT(a.name) AS similar 
-                FROM books b 
-                INNER JOIN authors a on b.author_id = a.id
-                WHERE (${where}) AND NOT a.name = '${autor}'
-                GROUP BY a.name
-                ORDER BY similar DESC"
-                );
-                $autors = mysqli_fetch_all($autors, MYSQLI_ASSOC);
-
-                //вывод в консоль
-                echo "Autor" . "\t\t" . "Similar" . "\n";
-                $this->printArray($autors);
-                die;
-            }
-
-            return ['errorMessage' => "Нет заданного автора в базе"];
-        } catch (Exception $e) {
-            return ['errorMessage' => $e->getMessage()];
-        }
+        $result = $this->_model->getSimilarAutor($autor);
+        //вывод в консоль
+        echo "Autor" . "\t\t" . "Similar" . "\n";
+        $this->printArray($result);
     }
 
     /**
